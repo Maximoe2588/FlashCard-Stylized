90ca5263e3d25db475ed2423ada20ac94472bc67
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-12-15 15:02:10 -0600

    update styling

--- src/AddCard/AddCardNavig.js
+++ src/AddCard/AddCardNavig.js
@@ -9,9 +9,9 @@ function AddCardNavig({deck}){
       <div className="container-navigation">
         <nav>
           <ol>
-            <li className="breadcrumb-primary"><a href="/">Home</a></li>
-            <li className="breadcrumb-primary"><a href={`/decks/${deck.id}`}>{deck.name}</a></li>
-            <li className="breadcrumb-secondary">Add Card</li>
+            <li className="breadcrumb primary"><a href="/">Home</a></li>
+            <li className="breadcrumb primary"><a href={`/decks/${deck.id}`}>{deck.name}</a></li>
+            <li className="breadcrumb secondary">Add Card</li>
           </ol>
         </nav>
       </div>
